a636252acc597a8b4f04bc7bb14d64600a4a619a
Terasuki <xt.wanglin@gmail.com>
2025-01-25 14:43:42 +0000

    add pages

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import json
 import sqlite3
-import pandas as pd
-import numpy as np
 import webbrowser
 
 from datetime import datetime
 from threading import Timer
 
-import plotly.express as px
 import dash_bootstrap_components as dbc
 
 from flask import Flask, request, jsonify, redirect
 from flask_cors import cross_origin
-from dash import Dash, html, dcc, callback, Output, Input, dash_table
+from dash import Dash, html, page_container, callback, Input, Output
 from dash_bootstrap_templates import load_figure_template
-
-from utilities import get_song_links, get_last_song_matches, get_previously_correct
 
 app = Flask(__name__)
 
@@ -24,7 +19,8 @@ dashboard = Dash(
     server=app,
     url_base_pathname='/main/',
     external_stylesheets=[dbc.themes.SLATE, '/assets/styles.css'],
-    update_title=None
+    update_title=None,
+    use_pages=True
 )
 
 dashboard.title = 'AMQ Song List'
@@ -123,196 +119,31 @@ def receive_data():
 def index():
     return redirect('/main')
 
-def app_description():
-    return html.Div(
-        id='app-description',
-        children=[
-            html.H2('Ranked Statistics'),
-        ]
-    )
-
-def last_song_anime():
-    return html.Div(
-        id='last_song_anime'
-    )
-
-def last_song_song():
-    return html.Div(
-        id='last_song_song'
-    )
-
-def last_song_artist():
-    return html.Div(
-        id='last_song_artist'
-    )
-
-def last_song_type():
-    return html.Div(
-        id='last_song_type'
-    )
-
-def last_song_links():
-    return html.Div(
-        id='last_song_links',
-        style={
-            'margin': 'auto',
-            'text-align': 'center'
-        }
-    )
-
-def last_song_difficulty():
-    return html.Div(
-        id='last_song_difficulty'
-    )
-
-def last_song_previously_correct():
-    return html.Div(
-        id='last_song_previously_correct',
-        style={
-            'margin': 'auto',
-            'text-align': 'center'
-        }
-    )
-
-def last_song_previously_played():
-    return html.Div(
-        dash_table.DataTable(
-            id='last_song_previously_played',
-            columns=[],
-            data=[],
-            style_as_list_view=True,
-            style_table={
-                'width': '100%', 
-                'overflowX': 'auto',
-                'overflowY': 'auto',
-                'border': 'thin lightgrey solid',
-            },
-            style_cell={
-                'textAlign': 'center',
-                'padding': '5px',
-            },
-            style_header={
-                'backgroundColor': 'rgb(30, 30, 30)',
-                'color': 'white'
-            },
-            style_data_conditional=[
-                {
-                    'if': {
-                        'filter_query': '{correct} = 1',
-                    },
-                    'backgroundColor': 'green',
-                    'color': 'white'
-                },
-                {
-                    'if': {
-                        'filter_query': '{correct} = 0',
-                    },
-                    'backgroundColor': 'red',
-                    'color': 'white'
-                },
-                {
-                    'if': {
-                        'filter_query': '{correct} is nil',
-                    },
-                    'backgroundColor': 'gray',
-                    'color': 'white'
-                },
-                {
-                    'if': {
-                        'filter_query': '{Guess time} is nil && {Answer} is blank',
-                    },
-                    'backgroundColor': 'gray',
-                    'color': 'white'
-                },
-                {'if': {'column_id': 'correct',},
-                'display': 'None',}
+dropdown = html.Div(
+    [
+        dbc.DropdownMenu(
+            [
+                dbc.DropdownMenuItem(
+                    'Last Song', href='/main/last-song'
+                ),
+                dbc.DropdownMenuItem(
+                    'All Songs', href='/main/all-songs'
+                ),
             ],
-            style_header_conditional=[
-                {'if': {'column_id': 'correct',},
-                    'display': 'None',}]
+            label='Navigation',
         ),
-        style={
-            'margin': 'auto',
-            'text-align': 'center'
-        }
-    )
+        
+    ],
+    style={
+        'margin': 'auto',
+        'text-align': 'center'
+    }
+)
 
-@callback(Output('last_song_anime', 'children'), 
-          Output('last_song_song', 'children'), 
-          Output('last_song_artist', 'children'), 
-          Output('last_song_type', 'children'), 
-          Output('last_song_difficulty', 'children'), 
-          Output('last_song_links', 'children'), 
-          Output('last_song_previously_correct', 'children'), 
-          Output('last_song_previously_played', 'columns'), 
-          Output('last_song_previously_played', 'data'), 
-          Input('interval', 'n_intervals'))
-def update(n):
-    conn = sqlite3.connect('data.db')
-    query = 'SELECT * FROM amq_data ORDER BY timestamp DESC LIMIT 1'
-    last_song = pd.read_sql_query(query, conn)
-    ls_matches = get_last_song_matches(last_song, conn)
-    conn.close()
-
-    links = get_song_links(last_song)
-    ls_anime = html.P(last_song.romaji_title, style={'text-align':'center'}, className='card-text')
-    ls_song = html.P(last_song.name, style={'text-align':'center'}, className='card-text')
-    ls_artist = html.P(last_song.artist, style={'text-align':'center'}, className='card-text')
-    ls_type = html.P(last_song.type, style={'text-align':'center'}, className='card-text')
-    ls_difficulty = html.P(last_song.difficulty, style={'text-align':'center'}, className='card-text')
-    ls_song_links = html.P([html.A(link['name'], href=link['url'], target='_blank', style={'margin-left': '7px', 'margin-right': '7px', 'text-align':'center'}) for link in links], className='card-text')
-    correct_guesses, wrong_guesses, spec_guesses = get_previously_correct(ls_matches)
-    ls_previously_correct = html.P(children=[html.Span(correct_guesses, style={'color':'green'}), 
-                                             '/',
-                                             html.Span(wrong_guesses, style={'color':'red'}),
-                                             '/',
-                                             html.Span(spec_guesses, style={'color':'gray'})], style={'text-align':'center'}, className='card-text')
-    ls_matches_data = ls_matches.to_dict('records')
-    ls_matches_columns = [{'name': col, 'id': col} for col in ls_matches.columns]
-
-    return ls_anime, ls_song, ls_artist, ls_type, ls_difficulty, ls_song_links, ls_previously_correct, ls_matches_columns, ls_matches_data
-
-dashboard.layout = dbc.Container(children=[
-
-            html.H2('Last Song', style={'text-align': 'center'}),
-
-            dbc.Row(dbc.Col(dbc.Card(dbc.CardBody([
-                html.H5('Anime', style={'text-align':'center'}, className='card-title'),
-                last_song_anime(),
-            ]), className="shadow-sm mb-1",))),
-            dbc.Row(dbc.Col(dbc.Card(dbc.CardBody([
-                html.H5('Song', style={'text-align':'center'}, className='card-title'),
-                last_song_song(),
-            ]), className="shadow-sm mb-1",))),
-            dbc.Row(dbc.Col(dbc.Card(dbc.CardBody([
-                html.H5('Artist', style={'text-align':'center'}, className='card-title'),
-                last_song_artist(),
-            ]), className="shadow-sm mb-1",))),
-            dbc.Row(dbc.Col(dbc.Card(dbc.CardBody([
-                html.H5('Type', style={'text-align':'center'}, className='card-title'),
-                last_song_type(),
-            ]), className="shadow-sm mb-1",))),
-            dbc.Row(dbc.Col(dbc.Card(dbc.CardBody([
-                html.H5('Links', style={'text-align':'center'}, className='card-title'),
-                last_song_links(),
-            ]), className="shadow-sm mb-1",))),
-            dbc.Row(dbc.Col(dbc.Card(dbc.CardBody([
-                html.H5('Difficulty', style={'text-align':'center'}, className='card-title'),
-                last_song_difficulty(),
-            ]), className="shadow-sm mb-1",))),
-            dbc.Row(dbc.Col(dbc.Card(dbc.CardBody([
-                html.H5('History', style={'text-align':'center'}, className='card-title'),
-                last_song_previously_correct(),   
-                last_song_previously_played(),
-            ]), className="shadow-sm mb-1",))),
-            dcc.Interval(
-                id='interval',
-                interval=500,
-                n_intervals=0
-            )
-        ], style={
-            'padding': '0.5em'
-        }),
+dashboard.layout = html.Div([
+    dropdown,
+    page_container
+])
 
 if __name__ == '__main__':
 
---
+++ pages/all_songs.py
@@ -0,0 +1,106 @@
+import sqlite3
+import pandas as pd
+
+import dash_bootstrap_components as dbc
+
+from dash import html, dcc, callback, Output, Input, dash_table, register_page
+
+from utilities import clean_full_data
+
+register_page(__name__)
+
+def main_table():
+    return html.Div(
+        dash_table.DataTable(
+            id='main_table',
+            columns=[],
+            data=[],
+            sort_action='native',
+            sort_mode='multi',
+            page_action='native',
+            page_size=50,
+            style_as_list_view=True,
+            style_table={
+                'width': '100%', 
+                'overflowX': 'auto',
+                'overflowY': 'auto',
+                'border': 'thin lightgrey solid',
+            },
+            style_cell={
+                'textAlign': 'center',
+                'padding': '5px',
+                'whiteSpace': 'normal',
+                'height': 'auto',
+            },
+            style_header={
+                'backgroundColor': 'rgb(30, 30, 30)',
+                'color': 'white'
+            },
+            style_data_conditional=[
+                {
+                    'if': {
+                        'filter_query': '{correct} = 1',
+                    },
+                    'backgroundColor': 'green',
+                    'color': 'white'
+                },
+                {
+                    'if': {
+                        'filter_query': '{correct} = 0',
+                    },
+                    'backgroundColor': 'red',
+                    'color': 'white'
+                },
+                {
+                    'if': {
+                        'filter_query': '{correct} is nil',
+                    },
+                    'backgroundColor': 'gray',
+                    'color': 'white'
+                },
+                {
+                    'if': {
+                        'filter_query': '{Guess time} is nil && {Answer} is blank',
+                    },
+                    'backgroundColor': 'gray',
+                    'color': 'white'
+                },
+                {'if': {'column_id': 'correct',},
+                'display': 'None',}
+            ],
+            style_header_conditional=[
+                {'if': {'column_id': 'correct',},
+                    'display': 'None',}]
+        ),
+        style={
+            'margin': 'auto',
+            'text-align': 'center'
+        }
+    )
+
+@callback(Output('main_table', 'columns'), 
+          Output('main_table', 'data'), 
+          Input('interval', 'n_intervals'))
+def update(n):
+    conn = sqlite3.connect('data.db')
+    query = """
+    SELECT timestamp, game_mode, romaji_title, name, artist, type, anime_type, vintage, difficulty, self_answer, guess_time, start_sample, video_length, ann_id, correct 
+    FROM amq_data
+    ORDER BY timestamp DESC"""
+    raw_data = pd.read_sql_query(query, conn)
+    conn.close()
+
+    data = clean_full_data(raw_data)
+    table_data = data.to_dict('records')
+    table_columns = [{'name': col, 'id': col} for col in data.columns]
+    return table_columns, table_data
+
+layout = dbc.Container(children=[
+    html.H2('All songs', style={'text-align': 'center'}),
+    main_table(),
+    dcc.Interval(
+        id='interval',
+        interval=5*6000,
+        n_intervals=0
+    )
+])
